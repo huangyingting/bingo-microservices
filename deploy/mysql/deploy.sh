29a0cb12c514e851554e97cd858009efb9142d52
huangyingting <huangyingting@outlook.com>
2022-05-31 17:09:07 +0800

    Update mysql

--- deploy/mysql/deploy.sh
+++ deploy/mysql/deploy.sh
@@ -1,7 +1,10 @@
 kubectl create namespace mysql
-helm install mysql azure-marketplace/mysql \
+helm install mysql bitnami/mysql \
+     --set architecture=replication \
      --set metrics.enabled=true \
      --set-string metrics.serviceMonitor.labels."release"="prometheus" \
      --set metrics.serviceMonitor.enabled=true \
+     --set auth.database="bingo" \
      --set auth.username="bingo" \
-     --set auth.password="P@ssw0rd" -n mysql
+     --set auth.password="P@ssw0rd" \
+     --set auth.replicationPassword="P@ssw0rd" -n mysql
